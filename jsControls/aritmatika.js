e6a803e9b4a6458e2d9afb5276acf442b4fe3e33
firmansyahci <firmansyah9dhe@gmail.com>
2019-12-19 22:51:17 +0700

    upload materi basic js

---
+++ jsControls/aritmatika.js
@@ -0,0 +1,6 @@
+const a = 10;
+const b = 12;
+
+console.log('Penambahan', a + b);
+
+console.log('Pengurangan', a - b);
---
+++ jsControls/relasi.js
@@ -0,0 +1,18 @@
+const a = 10;
+const b = 10;
+const c = 13;
+
+// lebih dari atau >
+console.log(a > b);
+
+// kurang dari atau <
+console.log(a < c);
+
+// sama dengan double atau = =
+// membandingkan nilai tanpa memperdulikan tipe data nya.
+console.log(a == '10');
+
+// sama dengan triple atau = = =
+// membandingkan nilai dan tipe data
+console.log(a === '10');
+
